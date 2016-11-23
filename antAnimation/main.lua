a5e53da655d75caceef6a265d884c7bbe389025b
Steph84 <st_lerendu@yahoo.fr>
2016-11-23 21:01:31 +0100

    rotate 90 deg _ draw lines for arcs

--- antAnimation/main.lua
+++ antAnimation/main.lua
@@ -3,9 +3,10 @@ io.stdout:setvbuf('no')
 love.graphics.setDefaultFilter("nearest")
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
-local windowWidth = 1200
+local windowWidth = 600
 local windowHeight = 600
 local whiteColor = {255, 255, 255}
+local coorPoints = {}
 
 listPoints = {}
 listPoints.rad = 0
@@ -31,46 +32,50 @@ level.c = 0
 level.l = 0
 level.gap = 0
 
+
+
   
 function love.load()
   love.window.setMode(windowWidth, windowHeight)
   love.graphics.setBackgroundColor(0, 102, 0)
   
   startPoint.gap = 50
-  startPoint.x = startPoint.gap
-  startPoint.y = windowHeight/2
+  startPoint.x = windowWidth/2
+  startPoint.y = startPoint.gap
   startPoint.rad = 10
   startPoint.seg = 6
   startPoint.col = {0, 128, 255}
 
   endPoint.gap = 50
-  endPoint.x = windowWidth-endPoint.gap
-  endPoint.y = windowHeight/2
+  endPoint.x = windowWidth/2
+  endPoint.y = windowHeight-endPoint.gap
   endPoint.rad = 10
   endPoint.seg = 6
   endPoint.col = {255, 128, 0}
     
   level.gap = 100
-  level.c = ((windowWidth - 2*startPoint.gap - 2*endPoint.gap)/level.gap) - 1
-  level.l = ((windowHeight)/level.gap) - 1
+  level.c = ((windowWidth)/level.gap) - 1
+  level.l = ((windowHeight - startPoint.gap - endPoint.gap)/level.gap) - 1
+  
   
   listPoints.rad = 10
   
   -- random creation of nodes
   local i
-  for i = 1, level.c do
+  for i = 1, level.l do
     tempTuple = {}
-    randL1 = love.math.random(level.l)
-    tempTuple = {randL1, i}
+    randL1 = love.math.random(level.c)
+    tempTuple = {i, randL1}
     table.insert(listPoints, tempTuple)
     tempTuple = {}
     repeat
-      randL2 = love.math.random(level.l)
+      randL2 = love.math.random(level.c)
     until randL2 ~= randL1
-    tempTuple = {randL2, i}
+    tempTuple = {i, randL2}
     table.insert(listPoints, tempTuple)
   end
   
+  -- initialisation level to 0
   local l, c, j
   for l = 1, level.l do
     level[l] = {}
@@ -78,11 +83,32 @@ function love.load()
       level[l][c] = 0
     end
   end
-  
+  -- point to 1
   for j = 1, #listPoints do
-    --print(listPoints[j][1], listPoints[j][2])
     level[listPoints[j][1]][listPoints[j][2]] = 1
   end
+  
+  local lbis, cbis
+  local bx, by = 0, 0
+  for lbis = 1, level.l do
+    bx = 0
+    for cbis = 1, level.c do
+      if level[lbis][cbis] == 1 then
+        tempPoint = {}
+        tempPoint = {bx + level.gap, by + level.gap + startPoint.gap}
+        table.insert(coorPoints, tempPoint)
+      end
+      bx = bx + level.gap
+    end
+    by = by + level.gap
+  end
+
+
+
+
+  tempRand = love.math.random(3, 4)
+ 
+
   
 end
 
@@ -98,19 +124,32 @@ function love.draw()
 
   love.graphics.setColor(whiteColor)
   
-  local l, c
-  local bx, by = 0, 0
-  for l = 1, level.l do
-    bx = 0
-    for c = 1, level.c do
-      if level[l][c] == 1 then
-        love.graphics.circle("fill", bx + 2*startPoint.gap + level.gap, by + level.gap, listPoints.rad) 
-      end
-      bx = bx + level.gap
-    end
-    by = by + level.gap
+  
+  local coorTuple, xyCoor
+  for coorTuple = 1, #coorPoints do
+    print("machin", coorPoints[coorTuple])
+    love.graphics.circle("fill", coorPoints[coorTuple][1], coorPoints[coorTuple][2], listPoints.rad)
   end
   
+  love.graphics.line(startPoint.x, startPoint.y, coorPoints[1][1], coorPoints[1][2])
+  love.graphics.line(startPoint.x, startPoint.y, coorPoints[2][1], coorPoints[2][2])
+  
+  love.graphics.line(coorPoints[1][1], coorPoints[1][2], coorPoints[3][1], coorPoints[3][2])
+  love.graphics.line(coorPoints[1][1], coorPoints[1][2], coorPoints[4][1], coorPoints[4][2])
+  love.graphics.line(coorPoints[2][1], coorPoints[2][2], coorPoints[3][1], coorPoints[3][2])
+  
+  love.graphics.line(coorPoints[3][1], coorPoints[3][2], coorPoints[5][1], coorPoints[5][2])
+  love.graphics.line(coorPoints[4][1], coorPoints[4][2], coorPoints[5][1], coorPoints[5][2])
+  love.graphics.line(coorPoints[4][1], coorPoints[4][2], coorPoints[6][1], coorPoints[6][2])
+  
+  love.graphics.line(coorPoints[5][1], coorPoints[5][2], coorPoints[7][1], coorPoints[7][2])
+  love.graphics.line(coorPoints[5][1], coorPoints[5][2], coorPoints[8][1], coorPoints[8][2])
+  love.graphics.line(coorPoints[6][1], coorPoints[6][2], coorPoints[8][1], coorPoints[8][2])
+  
+  love.graphics.line(endPoint.x, endPoint.y, coorPoints[7][1], coorPoints[7][2])
+  love.graphics.line(endPoint.x, endPoint.y, coorPoints[8][1], coorPoints[8][2])
+
+
 end
 
 function love.keypressed(key)
