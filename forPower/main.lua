b268d4e3f5b5ee21ef1f34b9e0e6f9f2e03e9c98
Pirate Captain <st_lerendu@yahoo.fr>
2016-12-27 10:15:33 +0100

    add cursor on top

--- forPower/main.lua
+++ forPower/main.lua
@@ -24,9 +24,12 @@ myColors.blue = {0, 0, 190}
 myColors.red = {255, 0, 0}
 myColors.yellow = {255, 255, 0}
 myColors.black = {0, 0, 0}
+myColors.green = {0, 255, 0}
 
 local coordTokens = {}
 
+cursorX = 0
+cursorY = 0
 
 function love.load()
   
@@ -61,11 +64,31 @@ function love.load()
     by = by + cell.height
   end
   
-  
-  
+  cursorY = cell.height - 20
+  print(cursorY)
 end
 
 function love.update(dt)
+  alpha = love.mouse.getX()
+  local tempCoord = cell.width
+  if alpha < tempCoord then
+    cursorX = tempCoord - tempCoord/2 - 5
+  elseif alpha < tempCoord * 2 then
+    cursorX = tempCoord * 2 - tempCoord/2 - 5
+  elseif alpha < tempCoord * 3 then
+    cursorX = tempCoord * 3 - tempCoord/2 - 5
+  elseif alpha < tempCoord * 4 then
+    cursorX = tempCoord * 4 - tempCoord/2 - 5
+  elseif alpha < tempCoord * 5 then
+    cursorX = tempCoord * 5 - tempCoord/2 - 5
+  elseif alpha < tempCoord * 6 then
+    cursorX = tempCoord * 6 - tempCoord/2 - 5
+  elseif alpha < tempCoord * 7 then
+    cursorX = tempCoord * 7 - tempCoord/2 - 5
+  end
+  --print(alpha)
+  --TODO
+  -- cursorX update
   
 end
 
@@ -83,14 +106,14 @@ function love.draw()
     end
   end
   
-  
-  
   local g
   for g = 1, #coordTokens do
-  
   love.graphics.circle("fill", coordTokens[g][1], coordTokens[g][2], token.d/2)
-  
   end
+
+  love.graphics.setColor(myColors.green)
+  love.graphics.rectangle("fill", cursorX, cursorY, 10, 10)
+
 end
 
 function love.keypressed(key)
