a72f5307ab55b58cd24132478fd80ab6b6c41f40
Pirate Captain <st_lerendu@yahoo.fr>
2017-01-10 23:57:22 +0100

    full movement of the fox run and jump

--- jamFoxAndDog/fox.lua
+++ jamFoxAndDog/fox.lua
@@ -1,24 +1,87 @@
 local fox = {}
 
-fox.pictures = {}
-local n
-for n=1, 7 do
-  fox.pictures[n] = love.graphics.newImage("pictures/fox_"..n..".png")
-end 
+local initJumpSpeed = 8.5
 
-fox.picCurrent = 1
-fox.coorX = 0
-fox.coorY = 0
+function fox.Load(pWindowHeight)
+  fox.pictures = {}
+  local n
+  for n=1, 7 do
+    fox.pictures[n] = love.graphics.newImage("pictures/fox_"..n..".png")
+  end 
 
-function fox.Update(dt)
-  fox.picCurrent = fox.picCurrent + (12 * dt)
+  fox.w = fox.pictures[1]:getWidth()
+  fox.h = fox.pictures[1]:getHeight()
+
+  fox.picCurrent = 1
+  fox.coorX = 0
+  fox.coorY = pWindowHeight - fox.h
+
+  fox.scX = 0.5
+  fox.scY = 0.5
+
+  fox.move = "right"
+  fox.jump = false
+  fox.jumpSpeed = initJumpSpeed
+
+end
+
+function fox.Update(dt, pWindowWidth, pWindowHeight)
+  -- animation of the fox
+  fox.picCurrent = fox.picCurrent + (12 * dt) -- using the delta time
   if math.floor(fox.picCurrent) > #fox.pictures then
     fox.picCurrent = 1
   end
+  
+  -- movement and postion of the fox
+  if fox.move == "right" then
+    fox.coorX = fox.coorX + (150 * dt)
+    fox.scX = 0.5
+  end
+  if fox.move == "left" then
+    fox.coorX = fox.coorX - (150 * dt)
+    fox.scX = 0 - 0.5
+  end
+  
+  -- change of the direction of the fox
+  if fox.coorX > (pWindowWidth - fox.w/4) then
+    fox.move = "left"
+  end
+  if fox.coorX < (0 + fox.w/4) then
+    fox.move = "right"
+  end
+  
+  -- manage the jump state of the fox
+  if love.keyboard.isDown("space") or fox.coorY < (pWindowHeight - fox.h - 10) then
+    fox.jump = true
+  end
+  if fox.coorY > (pWindowHeight - fox.h) then
+    fox.jump = false
+    fox.jumpSpeed = initJumpSpeed
+  end
+  
+  -- manage the coordinate changes of the fox
+  if fox.jump == false then
+    fox.coorY = pWindowHeight - fox.h
+  end
+  if fox.jump == true then
+    fox.coorY = fox.coorY - fox.jumpSpeed
+    fox.jumpSpeed = fox.jumpSpeed - dt*9.81
+  end
+  
+  
 end
 
-function fox.Draw()
-  love.graphics.draw(fox.pictures[math.floor(fox.picCurrent)], 100, 100, 0, 0.5, 0.5)
+function fox.Draw(pWindowHeight)
+  love.graphics.draw(
+                      fox.pictures[math.floor(fox.picCurrent)],
+                      fox.coorX,
+                      fox.coorY,
+                      0,
+                      fox.scX,
+                      fox.scY,
+                      fox.w/2,
+                      fox.h/2
+                    )
 end
 
 
--- jamFoxAndDog/main.lua
+++ jamFoxAndDog/main.lua
@@ -2,27 +2,30 @@ io.stdout:setvbuf('no')
 love.graphics.setDefaultFilter("nearest")
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
-local windowWidth = 800 -- default value
-local windowHeight = 600 -- default value
+local windowWidth = 1000 -- default value
+local windowHeight = 400 -- default value
 
 Fox = require("fox")
 
+local rectDepth = 50
 
 
 function love.load()
-  
   love.window.setMode(windowWidth, windowHeight)
-  
+  Fox.Load(windowHeight)
   
 end
 
 function love.update(dt)
-  Fox.Update(dt)
+  Fox.Update(dt, windowWidth, windowHeight)
 end
 
 function love.draw()
-  Fox.Draw()
+  love.graphics.setColor(255, 255, 255)
+  Fox.Draw(windowHeight)
   
+  love.graphics.setColor(0, 100, 0)
+  love.graphics.rectangle("fill", 0, windowHeight - rectDepth, windowWidth, rectDepth)
 end
 
 function love.keypressed(key)
