09720f2d34424253896dba01bdde52f32ea4bcf9
Pirate Captain <st_lerendu@yahoo.fr>
2017-01-10 21:49:57 +0100

    creation project for a jam

---
+++ jamFoxAndDog/main.lua
@@ -0,0 +1,29 @@
+io.stdout:setvbuf('no')
+love.graphics.setDefaultFilter("nearest")
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+local windowWidth = 800 -- default value
+local windowHeight = 600 -- default value
+
+local list_fox = {}
+
+function love.load()
+  
+  love.window.setMode(windowWidth, windowHeight)
+  
+  
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    
+end
+
+function love.keypressed(key)
+  
+  print(key)
+  
+end
