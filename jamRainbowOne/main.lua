2b72b88a54b646bcb0aa4e056301edd63935d35f
Pirate Captain <st_lerendu@yahoo.fr>
2017-01-17 23:12:00 +0100

    creation of a platform game the a jam

---
+++ jamRainbowOne/main.lua
@@ -0,0 +1,27 @@
+io.stdout:setvbuf('no')
+love.graphics.setDefaultFilter("nearest")
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+local windowWidth = 800 -- default value
+local windowHeight = 600 -- default value
+
+function love.load()
+  
+  love.window.setMode(windowWidth, windowHeight)
+  
+  
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    
+end
+
+function love.keypressed(key)
+  
+  print(key)
+  
+end
