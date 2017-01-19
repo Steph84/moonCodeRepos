8761aaad40a18d886ac043a29578169761213ed1
Pirate Captain <st_lerendu@yahoo.fr>
2017-01-19 23:13:50 +0100

    animation of the walk left and right

--- jamRainbowOne/character.lua
+++ jamRainbowOne/character.lua
@@ -1,16 +1,28 @@
 local Character = {}
 
 Character.stand = {}
-Character.walk = {}
+Character.walkR = {}
+Character.walkL = {}
 Character.dir = "right"
 Character.drawable = nil
+Character.picCurrent = 2
+Character.coorX = 100
 
 function Character.Load()
   Character.stand[1] = love.graphics.newImage("pictures/Rosette_Stand_R.png")
   Character.stand[2] = love.graphics.newImage("pictures/Rosette_Stand_L.png")
   
-  Character.walk[1] = love.graphics.newImage("pictures/Rosette_Walk_32x32_Anim_R.png")
-  Character.walk[2] = love.graphics.newImage("pictures/Rosette_Walk_32x32_Anim_L.png")
+  Character.walkR[1] = love.graphics.newImage("pictures/Rosette_walk_32x32_Anim_R.png")
+  Character.walkR[2] = love.graphics.newQuad(0, 0, 32, 32, Character.walkR[1]:getDimensions())
+  Character.walkR[3] = love.graphics.newQuad(32, 0, 32, 32, Character.walkR[1]:getDimensions())
+  Character.walkR[4] = love.graphics.newQuad(64, 0, 32, 32, Character.walkR[1]:getDimensions())
+  Character.walkR[5] = love.graphics.newQuad(96, 0, 32, 32, Character.walkR[1]:getDimensions())
+
+  Character.walkL[1] = love.graphics.newImage("pictures/Rosette_walk_32x32_Anim_L.png")
+  Character.walkL[2] = love.graphics.newQuad(0, 0, 32, 32, Character.walkL[1]:getDimensions())
+  Character.walkL[3] = love.graphics.newQuad(32, 0, 32, 32, Character.walkL[1]:getDimensions())
+  Character.walkL[4] = love.graphics.newQuad(64, 0, 32, 32, Character.walkL[1]:getDimensions())
+  Character.walkL[5] = love.graphics.newQuad(96, 0, 32, 32, Character.walkL[1]:getDimensions())
 
   Character.drawable = Character.stand[1]
 end
@@ -24,9 +36,17 @@ function Character.Update(dt)
   end
   
   if love.keyboard.isDown("right") and Character.dir == "right" then
-    -- walk 1
+    Character.picCurrent = Character.picCurrent + (12 * dt) -- using the delta time
+    Character.coorX = Character.coorX + 2
+    if math.floor(Character.picCurrent) > 5 then
+      Character.picCurrent = 2
+    end
   elseif love.keyboard.isDown("left") and Character.dir == "left" then
-    -- walk 2
+    Character.picCurrent = Character.picCurrent + (12 * dt) -- using the delta time
+    Character.coorX = Character.coorX - 2
+    if math.floor(Character.picCurrent) > 5 then
+      Character.picCurrent = 2
+    end
   elseif Character.dir == "right" then
     Character.drawable = Character.stand[1]
   elseif Character.dir == "left" then
@@ -38,7 +58,12 @@ function Character.Update(dt)
 end
 
 function Character.Draw()
-  love.graphics.draw(Character.drawable, 100, 640-6*32)
+  if Character.dir == "right" then
+    love.graphics.draw(Character.walkR[1], Character.walkR[math.floor(Character.picCurrent)], Character.coorX, 640-6*32)
+  elseif Character.dir == "left" then
+    love.graphics.draw(Character.walkL[1], Character.walkL[math.floor(Character.picCurrent)], Character.coorX, 640-6*32)
+  end
+  
 end
 
 
