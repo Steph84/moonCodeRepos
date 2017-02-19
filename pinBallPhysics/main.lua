a8a5f59c4f687d0ca11099543ba1e8a26a65c1a0
Pirate Captain <st_lerendu@yahoo.fr>
2017-02-19 18:51:27 +0100

    add ground and ball physics from tutorial

--- pinBallPhysics/main.lua
+++ pinBallPhysics/main.lua
@@ -11,9 +11,28 @@ if arg[#arg] == "-debug" then require("mobdebug").start() end
 local windowWidth = 800 -- default value
 local windowHeight = 600 -- default value
 
+local world, objects
+
 function love.load()
-  
   love.window.setMode(windowWidth, windowHeight)
+  
+  love.physics.setMeter(64) --the height of a meter our worlds will be 64px
+  world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
+
+  objects = {} -- table to hold all our physical objects
+ 
+  --let's create the ground
+  objects.ground = {}
+  objects.ground.body = love.physics.newBody(world, windowWidth/2, windowHeight-50/2) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
+  objects.ground.shape = love.physics.newRectangleShape(windowWidth, 50) --make a rectangle with a width of 650 and a height of 50
+  objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape) --attach shape to body
+ 
+  --let's create a ball
+  objects.ball = {}
+  objects.ball.body = love.physics.newBody(world, windowWidth/2, windowHeight/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
+  objects.ball.shape = love.physics.newCircleShape( 20) --the ball's shape has a radius of 20
+  objects.ball.fixture = love.physics.newFixture(objects.ball.body, objects.ball.shape, 1) -- Attach fixture to body and give it a density of 1.
+  objects.ball.fixture:setRestitution(0.9) --let the ball bounce
   
   
 end
