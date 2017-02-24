455d1385f245074e754700d4c755e2f16215a7dd
Pirate Captain <st_lerendu@yahoo.fr>
2017-02-24 22:26:24 +0100

    little star in orbit

--- jamStars/star.lua
+++ jamStars/star.lua
@@ -4,6 +4,9 @@ local Star = {}
 local listStars = {}
 local numStars = 1
 local starDiam = 5
+local target = {}
+local starSpeed = 2
+local F = 0
 
 function createStar(pId, pX, pY)
   local item = {}
@@ -15,8 +18,8 @@ function createStar(pId, pX, pY)
   item.id = pId
   item.x = pX
   item.y = pY
-  
-  item.vx = 0
+  F = tempRandX
+  item.vx = tempRandX
   item.vy = 0
   item.scX = 0.08
   item.scY = 0.08
@@ -26,27 +29,40 @@ function createStar(pId, pX, pY)
 end
 
 function Star.Load(pWindowWidth, pWindowHeight)
+  target.x = pWindowWidth/2
+  target.y = pWindowHeight/2
+  
   -- creation of the stars
   local i
   for i = 2, numStars + 1 do
     createStar(i, 600, pWindowHeight/2 + 100)
   end
 end
-  --fonction angle renvoyant l arctangente
-  function math.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
+
+--fonction angle renvoyant l arctangente
+function math.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
 
 function Star.Update(dt, pWindowWidth, pWindowHeight)
+  
   local i
   for i = 1, #listStars do
     local s = listStars[i]
+    
     s.x = s.x + s.vx
     s.y = s.y + s.vy
     
     local dSquare = math.pow((s.x - pWindowWidth/2), 2) + math.pow((s.y - pWindowHeight/2), 2)
     local gravityForce = 1/dSquare
-    s.vx = s.vx - gravityForce*100
+    s.vx = s.vx - gravityForce
+    
     local angle = math.angle(s.x, s.y, pWindowWidth/2, pWindowHeight/2)
-    print(angle*180/3.14)
+    local angleDeg = angle*(-1) + 3.14
+    print(angleDeg)
+    
+    s.vy = -F* math.cos(angleDeg)
+    s.vx = -F* math.sin(angleDeg)
+    print(s.vy)
+    
     
   end
 end
