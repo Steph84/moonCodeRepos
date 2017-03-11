40ed8ed5d2c92fee2c93ff8ccbed8d9cacd0eff1
Pirate Captain <st_lerendu@yahoo.fr>
2017-03-11 22:14:00 +0100

    add procedural stuff to create fragment

--- jamLightning/main.lua
+++ jamLightning/main.lua
@@ -8,10 +8,13 @@ local windowWidth = 800 -- default value
 local windowHeight = 600 -- default value
 
 local listFrag = {}
-local fragStep = 80
+local departure = 10
+local fragStep = 50
 local fragRange = 100
-local maxBranch = 3
+local nbBranch = 3
 local nbStep = windowHeight/fragStep
+local newSize = 0
+local oldSize = 0
 
 function createFrag(pX, pY, pVolt)
   local entity = {}
@@ -27,27 +30,47 @@ function love.load()
   love.window.setMode(windowWidth, windowHeight)
   local igniteFrag = {}
   igniteFrag.x1 = windowWidth/2
-  igniteFrag.y1 = 10
+  igniteFrag.y1 = departure
   igniteFrag.x2 = math.random(igniteFrag.x1 - fragRange, igniteFrag.x1 + fragRange)
   igniteFrag.y2 = igniteFrag.y1 + fragStep
-  igniteFrag.volt = 10
+  igniteFrag.volt = 12
   
   table.insert(listFrag, igniteFrag)
   
 end
 
 function love.update(dt)
-  if #listFrag < nbStep then
-    local i
-    for i = 1, nbStep do
-      local f = listFrag[i]
-      local nbBranch = math.random(1, maxBranch)
-      local j
-      for j = 1, nbBranch do
-        createFrag(f.x2, f.y2, f.volt-1)
+    if listFrag[#listFrag].y2 == departure + fragStep then
+      oldSize = #listFrag
+      local i
+      for i = 1, nbBranch do
+        local tempSizeList = #listFrag
+        createFrag(listFrag[ tempSizeList + 1 - i ].x2,
+                   listFrag[ tempSizeList + 1 - i ].y2,
+                   listFrag[ tempSizeList + 1 - i ].volt * ( i / nbBranch ) - 1)
+      end
+      newSize = #listFrag
+    end
+    
+    if listFrag[#listFrag].y2 == departure + 2 * fragStep then
+      local stepToCheck = newSize - oldSize
+      local a
+      local indexToCheck = {}
+      for a = #listFrag - stepToCheck, #listFrag do
+        if listFrag[a].volt > 2 then table.insert(indexToCheck, a) end
+      end
+      
+      local b
+      for b = 1, #indexToCheck do
+        local i
+        for i = 1, nbBranch do
+          createFrag(listFrag[indexToCheck[b]].x2,
+                     listFrag[indexToCheck[b]].y2,
+                     listFrag[indexToCheck[b]].volt * ( i / nbBranch ) - 1)
+        end
       end
     end
-  end
+    
 end
 
 function love.draw()
