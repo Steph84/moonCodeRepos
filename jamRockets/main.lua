41c6d3ba697f5c8fb59ebe2c27018cbe88d7f32c
Pirate Captain <st_lerendu@yahoo.fr>
2017-03-22 13:52:59 +0100

    add background picture and creation object files

--- jamRockets/main.lua
+++ jamRockets/main.lua
@@ -7,8 +7,17 @@ if arg[#arg] == "-debug" then require("mobdebug").start() end
 local windowWidth = 1300 -- max value
 local windowHeight = 675 -- max value
 
+local bg = {}
+
 function love.load()
   love.window.setMode(windowWidth, windowHeight)
+  bg.src = love.graphics.newImage("pictures/background.png")
+  bg.w = bg.src:getWidth()
+  bg.h = bg.src:getHeight()
+  bg.scale = windowWidth/bg.w
+  bg.x = 0
+  bg.y = windowHeight - bg.h * bg.scale/2
+  
   
 end
 
@@ -18,6 +27,18 @@ end
 
 function love.draw()
   
+  -- shading above the background picture
+  love.graphics.setColor(14, 11, 17)
+  love.graphics.rectangle("fill", 0, bg.y - 300, windowWidth, 100)
+  love.graphics.setColor(37, 21, 25)
+  love.graphics.rectangle("fill", 0, bg.y - 200, windowWidth, 100)
+  love.graphics.setColor(46, 24, 24)
+  love.graphics.rectangle("fill", 0, bg.y - 100, windowWidth, 100)
+  
+  -- 2 background pictures side to side
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(bg.src, bg.x, bg.y, 0, bg.scale/2, bg.scale/2)
+  love.graphics.draw(bg.src, bg.x + bg.w * bg.scale/2, bg.y, 0, bg.scale/2, bg.scale/2)
 end
 
 function love.keypressed(key)
