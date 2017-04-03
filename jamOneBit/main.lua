8a0b90b614eafcc1b04ea3908dcd492c1044069c
Pirate Captain <st_lerendu@yahoo.fr>
2017-04-03 14:42:43 +0200

    add second part of the button for the animation

--- jamOneBit/main.lua
+++ jamOneBit/main.lua
@@ -7,6 +7,8 @@ if arg[#arg] == "-debug" then require("mobdebug").start() end
 local windowWidth = 1300 -- max value
 local windowHeight = 675 -- max value
 
+local actualScreen = "title"
+local tempScreen = "title"
 local myTitle = require("title")
 
 function love.load()
@@ -14,16 +16,23 @@ function love.load()
   love.window.setMode(windowWidth, windowHeight)
   love.window.setTitle("RPG clicker")
   
-  myTitle.Load()
+  if actualScreen == "title" then myTitle.Load(windowWidth) end
+  
   
 end
 
 function love.update(dt)
-
+  if actualScreen == "title" then
+    --tempScreen = 
+    myTitle.Update(dt)
+    if tempScreen ~= "title" then actualScreen = tempScreen end
+  end
+  if actualScreen == "menu" then print("truc") end
 end
 
 function love.draw()
-  myTitle.Draw(windowWidth)
+  if actualScreen == "title" then myTitle.Draw(windowWidth) end
+  
 end
 
 function love.keypressed(key)
--- jamOneBit/title.lua
+++ jamOneBit/title.lua
@@ -4,21 +4,70 @@ local bgPic = nil
 local buttonPics = {}
 local buttonStand = {}
 local buttonPressed = {}
+local mouseClicked = {} 
+local animeButton = false
+local timeElapsed = 0
 
-function Title.Load()
+function Title.Load(pWindowWidth)
   bgPic = love.graphics.newImage("pictures/oneBitTitle.png")
-  buttonPics = love.graphics.newImage("pictures/oneBitTitleButtons.png")
-  buttonStand = love.graphics.newQuad(0, 0, 256, 64, buttonPics:getDimensions())
-  buttonPressed = love.graphics.newQuad(0, 64, 256, 64, buttonPics:getDimensions())
+  buttonPics.src = love.graphics.newImage("pictures/oneBitTitleButtons.png")
+  buttonPics.w = 256
+  buttonPics.h = 64
+  buttonPics.x = pWindowWidth/2
+  buttonPics.y = 550
+  
+  buttonStand = love.graphics.newQuad(0, 0, buttonPics.w, buttonPics.h, buttonPics.src:getDimensions())
+  buttonPressed = love.graphics.newQuad(0, buttonPics.h, buttonPics.w, buttonPics.h, buttonPics.src:getDimensions())
+end
+
+function love.mousepressed(x, y, button, istouch)
+  if button == 1 then
+    mouseClicked.on = true
+    mouseClicked.x = x
+    mouseClicked.y = y
+  end
+end
+
+function Title.Update(pDt)
+  
+  if mouseClicked.on == true then
+    if mouseClicked.x > buttonPics.x - buttonPics.w/2 and
+       mouseClicked.x < buttonPics.x + buttonPics.w/2 then
+         if mouseClicked.y > buttonPics.y - buttonPics.h/2 and
+            mouseClicked.y < buttonPics.y + buttonPics.h/2 then
+              animeButton = true
+              mouseClicked.x = nil
+              mouseClicked.y = nil
+              mouseClicked.on = false
+         end
+    end
+  end
+  
+  if animeButton == true then
+    timeElapsed = timeElapsed + pDt
+    if timeElapsed > 3 then
+      local signal = "menu"
+      return signal
+    end
+  end
+  
 end
 
 function Title.Draw(pWindowWidth)
   love.graphics.draw(bgPic, 0, 0)
-  love.graphics.draw(buttonPics, buttonStand,
-                     pWindowWidth/2, 550,
-                     0, 1, 1,
-                     256/2, 64/2)
-  -- TODO if button clicked then change pic to buttonPressed
+  
+  if animeButton == false then
+    love.graphics.draw(buttonPics.src, buttonStand,
+                       buttonPics.x, buttonPics.y,
+                       0, 1, 1,
+                       buttonPics.w/2, buttonPics.h/2)
+  elseif animeButton == true then
+    love.graphics.draw(buttonPics.src, buttonPressed,
+                       buttonPics.x, buttonPics.y,
+                       0, 1, 1,
+                       buttonPics.w/2, buttonPics.h/2)
+  end
+    -- TODO if button clicked then change pic to buttonPressed
   -- wait a little bit then switch to following screen
 
 end
