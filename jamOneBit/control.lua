57b55b67bde12f190c40a1878f2b514abdc862bd
Pirate Captain <st_lerendu@yahoo.fr>
2017-04-09 21:33:15 +0200

    add people management

--- jamOneBit/control.lua
+++ jamOneBit/control.lua
@@ -1,3 +1,5 @@
+-- initialiser le random
+math.randomseed(os.time())
 local Control = {}
 
 local buttonPics = {}
@@ -6,8 +8,15 @@ buttonPics.tileTextures = {}
 local tileWidth = 96
 local tileHeight = 48
 
+local peoplePics = {}
+
 local listButtons = {}
+local listPeople = {}
 local mouseClicked = {}
+local peopleCost = 10
+local peopleIncome = 10
+local peopleTime = 1
+local timeElapsed = 0
 
 function CreateButton(pId, pppWindowHeight)
   local item = {}
@@ -20,6 +29,47 @@ function CreateButton(pId, pppWindowHeight)
   item.position = "up"
   
   table.insert(listButtons, item)
+end
+
+function CreatePeople(pId)
+  local item = {}
+  
+  item.id = pId
+  -- 1st row Id 1, 2, 3, 4
+  if pId < 5 then
+    item.x = 85 + pId * 30
+    item.y = 100
+  end
+  -- 2nd row Id 5, 6, 7, 8
+  if pId > 4 and pId < 9 then
+    item.x = 85 + (pId - 4) * 30
+    item.y = 100 + 1 * 70
+  end
+  -- 3rd row Id 9, 10, 11, 12
+  if pId > 8 and pId < 13 then
+    item.x = 85 + (pId - 8) * 30
+    item.y = 100 + 2 * 70
+  end
+  -- 4th row Id 13, 14, 15, 16
+  if pId > 12 and pId < 17 then
+    item.x = 85 + (pId - 12) * 30
+    item.y = 100 + 3 * 70
+  end
+  -- 5th row Id 17, 18, 19, 20
+  if pId > 16 and pId < 21 then
+    item.x = 85 + (pId - 16) * 30
+    item.y = 100 + 4 * 70
+  end
+  -- 6th row Id 21, 22, 23, 24
+  if pId > 20 and pId < 25 then
+    item.x = 85 + (pId - 20) * 30
+    item.y = 100 + 5 * 70
+  end
+  
+  local gender = math.random(1, 2)
+  if gender == 1 then item.gender = "male" else item.gender = "female" end
+  
+  table.insert(listPeople, item)
 end
 
 function Control.Load(ppWindowHeight)
@@ -44,6 +94,8 @@ function Control.Load(ppWindowHeight)
     end
   end
   
+  peoplePics.male = love.graphics.newImage("pictures/maleBlack.png")
+  peoplePics.female = love.graphics.newImage("pictures/femaleBlack.png")
   
   local j
   for j = 1, 6 do
@@ -71,28 +123,47 @@ function love.mousereleased(x, y, button, istouch)
 end
 
 function Control.Update(ppDt, pIncrement)
-  
-  local i
-  for i = 1, #listButtons do
-    local b = listButtons[i]
-    --if b.isEnabled == true then -- if the button is showing
-      if mouseClicked.on == true then -- if the player click
+  if mouseClicked.on == true then -- if the player click
+    local i
+    for i = 1, #listButtons do
+      local b = listButtons[i]
+      if b.isEnabled == true then -- if the button is showing
         if mouseClicked.x > b.x - tileWidth/2 and
            mouseClicked.x < b.x + tileWidth/2 then
              if mouseClicked.y > b.y - tileHeight/2 and
                 mouseClicked.y < b.y + tileHeight/2 then
+                  
                   b.position = "down" -- if the button is clicked, button down
+                  
                   if b.id == 1 then
                     pIncrement = pIncrement + 1
                     -- to stop increment without stopping animation
                     mouseClicked.x = 0
                     mouseClicked.y = 0
                   end
+                  
+                  if b.id == 2 then
+                    pIncrement = pIncrement - peopleCost
+                    CreatePeople(#listPeople + 1)
+                    mouseClicked.x = 0
+                    mouseClicked.y = 0
+                  end
+                  
+                  
              end
         end
       else b.position = "up" end -- if the player doesn t click, button back up
-    --end
+    end
   end
+  
+  -- manage the showing of the people button
+  if pIncrement > peopleCost and #listPeople < 24 then listButtons[2].isEnabled = true else listButtons[2].isEnabled = false end
+  if #listPeople > 0 then timeElapsed = timeElapsed + ppDt end
+  if timeElapsed > peopleTime then
+    pIncrement = pIncrement + #listPeople * peopleIncome
+    timeElapsed = 0
+  end
+  
   
   return pIncrement
 end
@@ -103,7 +174,7 @@ function Control.Draw()
   for i = 1, #listButtons do
     local b = listButtons[i]
     -- manage the showing of the button
-    --if b.isEnabled == true then
+    if b.isEnabled == true then
       local tempTextId
       -- manage the animation up and down of the button
       if b.position == "up" then tempTextId = b.id elseif b.position == "down" then tempTextId = b.id + 6 end
@@ -112,8 +183,14 @@ function Control.Draw()
                          b.x, b.y,
                          0, 1, 1,
                          tileWidth/2, tileHeight/2)
-    --end
+    end
+  end
   
+  local j
+  for j = 1, #listPeople do
+    local p = listPeople[j]
+    if p.gender == "male" then love.graphics.draw(peoplePics.male, p.x, p.y) end
+    if p.gender == "female" then love.graphics.draw(peoplePics.female, p.x, p.y) end
   end
 end
 
