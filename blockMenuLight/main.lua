ada0c0005ee69bfff45db04d4655871f0b180a5a
Pirate Captain <st_lerendu@yahoo.fr>
2017-06-01 13:22:32 +0200

    add blockMenuLight without options settings

---
+++ blockMenuLight/main.lua
@@ -0,0 +1,36 @@
+io.stdout:setvbuf('no')
+love.graphics.setDefaultFilter("nearest")
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+local windowWidth = 800
+local windowHeight = 600
+
+local gameState = "menu"
+local reload = false
+
+local myMenu = require("menu")
+
+function love.load()
+  
+  love.window.setMode(windowWidth, windowHeight)
+  love.window.setTitle("my Title")
+  
+  myMenu.Load(windowWidth, windowHeight)
+  
+end
+
+function love.update(dt)
+  
+  if gameState == "menu" then
+    myMenu.Update(dt)
+  end
+  
+end
+
+function love.draw()
+  
+  if gameState == "menu" then
+    myMenu.Draw()
+  end
+  
+end
---
+++ blockMenuLight/menu.lua
@@ -0,0 +1,148 @@
+local Menu = {}
+
+Menu.menuState = nil
+
+local windowWidth, windowHeight, gameVersion, anchorTitleY, anchorSelectionY
+local selectionItems = {}
+local itemFonts = {}
+local soundObjects = {}
+
+local myPicture = require("loadPictures")
+local myCredits = require("menuCredits")
+
+function Menu.Load(pWindowWidth, pWindowHeight)
+  windowWidth = pWindowWidth
+  windowHeight = pWindowHeight
+  
+  Menu.menuState = "title"
+  gameVersion = "v1.0"
+  
+  itemFonts.fontSize = 32
+  itemFonts.titles = itemFonts.fontSize * 2
+  itemFonts.selections = itemFonts.fontSize * 1
+  itemFonts.version = itemFonts.fontSize * 0.75
+  itemFonts.credits = itemFonts.fontSize * 0.75
+  
+  anchorTitleY = windowHeight*0.05
+  anchorSelectionY = windowHeight*0.5
+
+  -- list of items of the menu
+  selectionItems.data = { "New game", "Credits", "Quit" } -- Load Game
+  -- which item is selected on opening
+  selectionItems.itemSelected = 1
+
+  -- load the different parts of the menu block
+  myPicture.Load()
+  myCredits.Load(windowWidth, windowHeight, itemFonts.credits)
+  
+  -- load the sound effects
+  soundObjects.selectionMove = love.audio.newSource("sounds/moveSelect.wav", "static")
+  soundObjects.selectionMove:setVolume(0.25)
+  soundObjects.selectionValidate = love.audio.newSource("sounds/validateSelect.wav", "static")
+  soundObjects.selectionValidate:setVolume(0.25)
+  soundObjects.back = love.audio.newSource("sounds/headBack.wav", "static")
+  soundObjects.back:setVolume(0.5)
+  
+  -- load the background music
+  soundObjects.bgMusic = love.audio.newSource("musics/dkSNESTheme.mp3", "stream")
+  soundObjects.bgMusic:setLooping(true)
+  soundObjects.bgMusic:setVolume(0.25)
+  --soundObjects.bgMusic:play()
+end
+
+
+function Menu.Update(dt)
+  
+  if Menu.menuState == "title" then
+    -- manage the looping selection on the menu
+    local optionsLength = #selectionItems.data
+    if selectionItems.itemSelected < 1 then
+      selectionItems.itemSelected = selectionItems.itemSelected + optionsLength
+    end
+    if selectionItems.itemSelected > optionsLength then
+      selectionItems.itemSelected = selectionItems.itemSelected - optionsLength
+    end
+  end
+  
+  if Menu.menuState == "credits" then
+    Menu.menuState = myCredits.Update(dt, Menu.menuState)
+    if Menu.menuState == "title" then soundObjects.back:play() end
+  end
+  
+end
+
+function Menu.Draw()
+  
+  if Menu.menuState == "title" then
+    -- draw the title and subtitle
+    love.graphics.setColor(0, 0, 255)
+    love.graphics.setFont(love.graphics.newFont("fonts/Capture_it.ttf", itemFonts.titles))
+    love.graphics.printf("SALEM", 0, anchorTitleY, windowWidth, "center")
+    
+    love.graphics.setColor(255, 0, 255)
+    love.graphics.setFont(love.graphics.newFont("fonts/AlexBrush-Regular.ttf", itemFonts.titles))
+    love.graphics.printf("Story", 0, anchorTitleY + itemFonts.titles, windowWidth, "center")
+    
+    -- draw the menu selection
+    love.graphics.setFont(love.graphics.newFont("fonts/Pacifico.ttf", itemFonts.selections))
+    local i
+    for i = 1, #selectionItems.data do
+      local msg = selectionItems.data[i]
+      
+      -- highlight the selected item
+      if i == selectionItems.itemSelected then
+        msg = "> "..msg.." <"
+        selectionItems.color = {255, 255, 255}
+      else selectionItems.color = {120, 120, 120} end
+      
+      love.graphics.setColor(selectionItems.color)
+      love.graphics.printf(msg, 0, anchorSelectionY + 1.5*(i-1) * itemFonts.selections, windowWidth, "center")
+    end
+    
+    -- draw the game version
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.setFont(love.graphics.newFont("fonts/Times_New_Roman_Normal.ttf", itemFonts.version))
+    love.graphics.printf(gameVersion, 0, windowHeight - itemFonts.version, windowWidth, "right")
+  end
+  
+  if Menu.menuState == "credits" then myCredits.Draw() end
+  
+end
+
+-- using keypressed function ponctual action
+function love.keypressed(key, isRepeat)
+  if Menu.menuState == "title" then
+    -- manage the looping navigation through the menu
+    if key == "up" then
+      soundObjects.selectionMove:stop() -- avoid to overlap the sounds
+      soundObjects.selectionMove:play()
+      selectionItems.itemSelected = selectionItems.itemSelected - 1
+    end
+    if key == "down" then
+      soundObjects.selectionMove:stop() -- avoid to overlap the sounds
+      soundObjects.selectionMove:play()
+      selectionItems.itemSelected = selectionItems.itemSelected + 1
+    end
+    
+    -- manage the selection
+    if key == "return" then
+      
+      -- avoid to overlap the sounds
+      soundObjects.selectionMove:stop()
+      soundObjects.selectionValidate:stop()
+      soundObjects.selectionValidate:play()
+      
+      if selectionItems.itemSelected == 1 then Menu.menuState = "game" end -- start a new game
+      if selectionItems.itemSelected == #selectionItems.data then
+        love.timer.sleep(0.6) -- wait a moment to heard the sound effect
+        love.event.quit() -- exit the game
+      end
+      
+      -- if itemSelected == 2 then gameState = "loadGame" end
+      if selectionItems.itemSelected == 2 then Menu.menuState = "credits" end
+    end
+  end
+  
+end
+
+return Menu
