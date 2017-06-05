15b3d2575fe4c1a466ef75d54db42e188d35b788
Pirate Captain <st_lerendu@yahoo.fr>
2017-06-05 22:39:35 +0200

    add gameState game and file

---
+++ jamMirror/game.lua
@@ -0,0 +1,21 @@
+local Game = {}
+
+local windowWidth, windowHeight
+
+function Game.Load(pWindowWidth, pWindowHeight)
+  windowWidth = pWindowWidth
+  windowHeight = pWindowHeight
+  
+  
+end
+
+function Game.Update(dt, pGameState)
+  
+  return pGameState
+end
+
+function Game.Draw()
+  
+end
+
+return Game
--- jamMirror/main.lua
+++ jamMirror/main.lua
@@ -3,24 +3,26 @@ io.stdout:setvbuf('no')
 love.graphics.setDefaultFilter("nearest")
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
-local windowWidth = 1280 -- 32x32px sprites value (40 columns)
-local windowHeight = 672 -- 32x32px sprites value (21 lines)
+local windowWidth, windowHeight
 
 local gameState = "menu"
 
 local myParallax = require("parallaxAnimation")
 local myMenu = require("menu")
+local myGame = require("game")
 
 local timeElpased = 0
 local drawParallax, drawMenu = true, false
 
 function love.load()
   
-  love.window.setMode(windowWidth, windowHeight)
+  love.window.setFullscreen(true)
+  windowWidth, windowHeight = love.graphics.getDimensions()
   love.window.setTitle("Into the Mirror")
   
   myParallax.Load(windowWidth, windowHeight)
   myMenu.Load(windowWidth, windowHeight)
+  myGame.load(windowWidth, windowHeight)
   
 end
 
@@ -42,19 +44,28 @@ function love.update(dt)
     
     if drawParallax == true then myParallax.Update(dt) end
     if drawMenu == true then myMenu.Update(dt) end
-  
+    
+    if myMenu.menuState == "game" then gameState = "game" end
   end
+  
+  if gameState == "game" then
+    gameState = myGame.Update(dt, gameState)
+  end
+  
 end
 
 function love.draw()
   
   if gameState == "menu" then
-    
     if drawParallax == true then myParallax.Draw() end
     if drawMenu == true then myMenu.Draw() end
   else 
     drawMenu = false
     drawParallax = false
+  end
+  
+  if gameState == "game" then
+    myGame.Draw()
   end
   
 end
