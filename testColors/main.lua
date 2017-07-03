9e11f0f595e80b58dddd78911605aa1f89034829
Pirate Captain <st_lerendu@yahoo.fr>
2017-07-03 13:00:16 +0200

    adjust color values

--- testColors/main.lua
+++ testColors/main.lua
@@ -25,18 +25,20 @@ function love.load()
   end
   
   listColors[10].color = {255, 0, 0} -- red
-  listColors[2].color = {42, 96, 153} -- blue
-  listColors[6].color = {255, 255, 0} -- yellow
-  listColors[4].color = {0, 169, 51} -- green
-  listColors[8].color = {255, 128, 0} -- orange
-  listColors[12].color = {128, 0, 128} -- purple
-  listColors[3].color = {21, 132, 102} -- teal
-  listColors[7].color = {255, 191, 0} -- amber
-  listColors[5].color = {129, 212, 26} -- chartreuse
   listColors[9].color = {255, 64, 0} -- vermilion
-  listColors[1].color = {85, 48, 141} -- violet
-  listColors[11].color = {191, 0, 65} -- cinnabar
-
+  listColors[8].color = {255, 128, 0} -- orange
+  listColors[7].color = {255, 192, 0} -- amber
+  listColors[6].color = {255, 255, 0} -- yellow
+  
+  listColors[11].color = {192, 0, 64} -- cinnabar
+  listColors[12].color = {128, 0, 128} -- purple
+  listColors[1].color = {64, 0, 192} -- violet
+  listColors[2].color = {0, 0, 255} -- blue
+  
+  listColors[3].color = {0, 128, 128} -- teal
+  listColors[4].color = {0, 255, 0} -- green
+  listColors[5].color = {128, 255, 0} -- chartreuse
+  
 end
 
 function love.update(dt)
