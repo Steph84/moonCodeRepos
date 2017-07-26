5d6a1ac10632ad7d2c6831415bd2f39ef4dd6c1d
Pirate Captain <st_lerendu@yahoo.fr>
2017-07-26 21:27:51 +0200

    hero hit ok + enemy hit ok

--- pixelPlatform/combat.lua
+++ pixelPlatform/combat.lua
@@ -11,29 +11,27 @@ function Combat.Update(dt)
   
   -- detection hero is hitted
   if myHero.attack == false and myHero.hitted == false then
-    if ( (myHero.xLeft >= myEnemy.x and myHero.xLeft <= (myEnemy.x + myEnemy.w * myEnemy.scale))
-        or (myHero.xRight >= myEnemy.x and myHero.xRight <= (myEnemy.x + myEnemy.w * myEnemy.scale)) )
-      and ( (myHero.yHead >= myEnemy.y and myHero.yHead <= (myEnemy.y + myEnemy.h * myEnemy.scale))
-        or (myHero.yFeet >= myEnemy.y and myHero.yFeet <= (myEnemy.y + myEnemy.h * myEnemy.scale)) ) then
-          --myHero.hitted = true
-    end
-  end
-  
-  local dx = myHero.xLeft - myEnemy.x
-  local dy = myHero.yHead - myEnemy.y
-  if (math.abs(dx) < ((myHero.xRight - myHero.xLeft) + myEnemy.w / myEnemy.scale)) then
-    if (math.abs(dy) < ((myHero.yFeet - myHero.yHead) + myEnemy.h / myEnemy.scale)) then
-      myHero.hitted = true
+    
+    local dx = myHero.xHead - myEnemy.x
+    local dy = myHero.yHead - myEnemy.y
+    if (math.abs(dx) < (myEnemy.w / myEnemy.scale)) then
+      if (math.abs(dy) < ((myHero.yFeet - myHero.yHead) + (myEnemy.h / myEnemy.scale))) then
+        myHero.hitted = true
+      end
     end
   end
   
   -- detection enemy is hitted
   if myHero.attack == true and myEnemy.hitted == false then
-    if ( (myHero.x >= myEnemy.x and myHero.x <= (myEnemy.x + myEnemy.w * myEnemy.scale))
-        or ((myHero.x + myHero.w * myHero.scale) >= myEnemy.x and (myHero.x + myHero.w * myHero.scale) <= (myEnemy.x + myEnemy.w * myEnemy.scale)) )
-      and ( (myHero.yHead >= myEnemy.y and myHero.yHead <= (myEnemy.y + myEnemy.h * myEnemy.scale))
-        or (myHero.yFeet >= myEnemy.y and myHero.yFeet <= (myEnemy.y + myEnemy.h * myEnemy.scale)) ) then
-          myEnemy.hitted = true
+    local dx
+    if myHero.sign == 1 then
+      dx = myHero.x - myEnemy.x
+    else dx = myHero.x - myEnemy.x end
+    local dy = myHero.y - myEnemy.y
+    if (math.abs(dx) < (myHero.w + (myEnemy.w / myEnemy.scale))) then
+      if (math.abs(dy) < ((myHero.yFeet - myHero.yHead) + (myEnemy.h / myEnemy.scale))) then
+        myEnemy.hitted = true
+      end
     end
   end
   
