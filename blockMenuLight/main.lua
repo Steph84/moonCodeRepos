39ecb93a103ef768709b07874e4ac2d5427c27c1
Pirate Captain <st_lerendu@yahoo.fr>
2017-07-28 09:33:06 +0200

    update manu light with dynamic window size in relation to the display size

--- blockMenuLight/main.lua
+++ blockMenuLight/main.lua
@@ -4,12 +4,19 @@ if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 local windowWidth = 800
 local windowHeight = 600
+local TILE_SIZE = 32
 
 local gameState = "menu"
 
 local myMenu = require("menu")
 
 function love.load()
+  
+  -- to adapt the window size to the display size
+  local displayWidth, displayHeight
+  displayWidth, displayHeight = love.window.getDesktopDimensions()
+  windowWidth = (math.floor(displayWidth/TILE_SIZE) - 2) * TILE_SIZE
+  windowHeight = (math.floor(displayHeight/TILE_SIZE) - 3) * TILE_SIZE
   
   love.window.setMode(windowWidth, windowHeight)
   love.window.setTitle("my Title")
