fa01153f88743d5ea92d95f9cedcd0ec15394a78
Pirate Captain <st_lerendu@yahoo.fr>
2017-09-15 23:46:22 +0200

    finish resolution test - found it

--- testResize/main.lua
+++ testResize/main.lua
@@ -12,21 +12,21 @@ local windowHeight = 576 -- default value 18 lines (2 for the hud)
 
 local Map = {}
 Map.grid = {
-              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
-              {0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
-              {0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0},
-              {0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0},
-              {0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0},
-              {0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0},
-              {0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0},
-              {0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0},
-              {0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-              {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-              {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-              {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
               {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6}
            }
 
@@ -36,46 +36,36 @@ local reInitVY = 10
 local Cube = {}
 
 local myColor = require("color")
+local displayWidth, displayHeight
 
 function love.load()
+  
+  displayWidth, displayHeight = love.window.getDesktopDimensions()
+  if displayWidth > 1500 then
+    windowWidth = windowWidth * 1.5
+    windowHeight = windowHeight * 1.5
+  end
   
   love.window.setMode(windowWidth, windowHeight)
   love.window.setTitle("my Title")
   
   -- initialize the size of the map
-  Map.size = { w = windowWidth/TILE_SIZE, h = (windowHeight - (2 * TILE_SIZE))/TILE_SIZE,
-               pixW = windowWidth, pixH = windowHeight - (2 * TILE_SIZE)}
+  Map.size = { w = 960/TILE_SIZE, h = (576 - (2 * TILE_SIZE))/TILE_SIZE}
   
-  --[[
-  -- building the map
-  local lin, col
-  local idGrid = 0
-  for lin = Map.size.h, 1, -1 do
-    Map.grid[lin] = {}
-    for col = 1, Map.size.w do
-      idGrid = idGrid + 1
-      -- initialize the map
-      Map.grid[lin][col] = {id = idGrid,
-                            x = (col-1)*TILE_SIZE, y = (lin-1)*TILE_SIZE,
-                            w = TILE_SIZE, h = TILE_SIZE, 
-                            idText = math.random(1, 37), texture = "void", scale = scale}
-      
-    end
-  end
-  --]]
+ 
   
   Cube.x = 100
   Cube.y = 100
   Cube.size = 32
   Cube.vx = 5
-  Cube.vy = reInitVY
+  Cube.vy = 5
   Cube.state = "stand"
   
 end
 
 local function getTileAt(pX, pY)
-  local col = math.floor(pX / 32) + 1
-  local lig = math.floor(pY / 32) + 1
+  local col = math.floor(pX / TILE_SIZE) + 1
+  local lig = math.floor(pY / TILE_SIZE) + 1
   if col > 0 and col <= #Map.grid[1] and lig > 0 and lig <= #Map.grid then
     local id = Map.grid[lig][col]
     return id
@@ -88,12 +78,10 @@ local function isSolid(pID)
   if pID > 0 then return true end
 end
 
-local function Collide(pSprite)
-  local id = getTileAt(pSprite.x + TILE_SIZE/2, pSprite.y + TILE_SIZE/2)
-  return isSolid(id)
-end
-
 function love.update(dt)
+  
+  local textureUnder = getTileAt(Cube.x + TILE_SIZE/2, Cube.y + TILE_SIZE + 5)
+  
   if love.keyboard.isDown("right") then
     Cube.x = Cube.x + Cube.vx
     Cube.state = "walk"
@@ -104,8 +92,12 @@ function love.update(dt)
     Cube.state = "stand"
   end
     
-  if (love.keyboard.isDown("space") and Cube.state ~= "jump") or Cube.y < Map.size.pixH then
+  if (love.keyboard.isDown("space") and Cube.state ~= "jump") or textureUnder == 0 then
     Cube.state = "jump"
+  end
+  
+  if (love.keyboard.isDown("right") or love.keyboard.isDown("left")) and textureUnder == 0 then
+    --Cube.vy = 0
   end
   
   if Cube.state == "jump" then
@@ -115,29 +107,17 @@ function love.update(dt)
     Cube.vy = reInitVY
   end
   
-  local collide = false
-  if Cube.vx ~= 0 then collide = Collide(Cube) end
-
-  if collide then
-    Cube.vx = 0
-    local col = math.floor((Cube.x + 16) / 32) + 1
-    Cube.x = (col-1)*16
-  end
-
-  if Cube.vy ~= 0 then
-    collide = Collide(Cube)
-    if collide then
-      Cube.vy = 0
-      local lig = math.floor((Cube.y + 16) / 32) + 1
-      Cube.y = (lig-1)*16
-    end
-  end
-
   
   
 end
 
 function love.draw()
+  
+  if displayWidth > 1500 then
+    love.graphics.scale(1.5, 1.5)
+  end
+  
+  
   local lin, col
   for lin = 1, Map.size.h do
     for col = 1, Map.size.w do
@@ -150,5 +130,8 @@ function love.draw()
   end
   love.graphics.setColor(255, 255, 255)
   love.graphics.rectangle("fill", Cube.x, Cube.y, Cube.size, Cube.size)
+  
+  love.graphics.circle("fill", Cube.x + TILE_SIZE/2, Cube.y + TILE_SIZE + 5, 5)
+  
   
 end
