8c59677cf6c271ce2459916c74c52368808d935a
Pirate Captain <st_lerendu@yahoo.fr>
2018-06-06 00:23:50 +0200

    add change of phase + showing + a lot of stuff

--- jamDimension/game.lua
+++ jamDimension/game.lua
@@ -6,12 +6,18 @@ BlackHole.src = {}
 
 local objectNumber = 10
 local listObjects = {}
+local bgMusic = {}
+Game.duration = 0
+Game.timeElapsed = 0
 
-function createObject(id, windowWidth, windowHeight)
+function createObject(id, windowWidth, windowHeight, pNature)
   local item = {}
+  item.nature = Game.phase
+  if pNature ~= nil then
+    item.nature = pNature
+  end
   
   item.id = id
-  item.nature = nature
   item.x = windowWidth/2
   item.y = windowHeight/2
   
@@ -32,13 +38,13 @@ function createObject(id, windowWidth, windowHeight)
   item.scaleX = 1
   item.scaleY = 1
   
-  if Game.phase == "0D" then
+  if item.nature == 0 then
     item.circSize = 5
-  elseif Game.phase == "1D" then
+  elseif item.nature == 1 then
     item.lineSize = 50
     item.x2 = math.random(item.x - item.lineSize, item.x + item.lineSize)
     item.y2 = math.random(item.y - item.lineSize, item.y + item.lineSize)
-  elseif Game.phase == "2D" then
+  elseif item.nature == 2 then
     item.size = 100
     item.vertexNb = math.random(3, 5)
     item.vertices = {}
@@ -60,7 +66,7 @@ end
 function Game.Load(GameSizeCoefficient, pWindowWidth, pWindowHeight)
   windowWidth = pWindowWidth
   windowHeight = pWindowHeight
-  Game.phase = "2D"
+  Game.phase = 0
   
   BlackHole.src = love.graphics.newImage("pictures/blackHole.png")
   BlackHole.x = windowWidth/2
@@ -75,10 +81,26 @@ function Game.Load(GameSizeCoefficient, pWindowWidth, pWindowHeight)
   for i = 1, objectNumber do
     createObject(i, windowWidth, windowHeight)
   end
+  
+  -- load the background music
+  bgMusic = love.audio.newSource("musics/reptileSong.wav", "stream")
+  bgMusic:setLooping(false)
+  bgMusic:setVolume(0.75)
+  --bgMusic:play()
+  Game.duration = bgMusic:getDuration() -- in seconds
+  
 end
 
 
 function Game.Update(dt)
+  
+  -- manage the phases in relation to the time
+  if Game.timeElapsed > 125 and Game.timeElapsed < 280 then
+    Game.phase = 1
+  elseif Game.timeElapsed >= 280 then
+    Game.phase = 2
+  end
+  
   
   -- oscillation of the blackHole
   if BlackHole.move == "forth" then
@@ -95,9 +117,9 @@ function Game.Update(dt)
     end
   end
   
-  while #listObjects < objectNumber do
-    createObject(#listObjects + 1, windowWidth, windowHeight)
-  end
+  --while #listObjects < objectNumber do
+    --createObject(#listObjects + 1, windowWidth, windowHeight)
+  --end
   
   -- blackHole movements
   if love.keyboard.isDown("down") then
@@ -122,17 +144,17 @@ function Game.Update(dt)
     local o = listObjects[i]
     
     -- objects AI
-    if Game.phase ~= "2D" then
+    if o.nature ~= 2 then
       o.x = o.x + o.vx
       o.y = o.y + o.vy
     end
     
-    if Game.phase == "1D" then
+    if o.nature == 1 then
       o.x2 = o.x2 + o.vx
       o.y2 = o.y2 + o.vy
     end
     
-    if Game.phase == "2D" then
+    if o.nature == 2 then
       for j = 1, #o.vertices do
         --local coor = o.vertices[j]
         if (j % 2 == 0) then
@@ -147,7 +169,7 @@ function Game.Update(dt)
     
     
     -- collision with blackHole
-    if Game.phase == "2D" then
+    if Game.phase == 2 then
       
       if math.abs(o.vertices[1] - BlackHole.x) < 25 and math.abs(o.vertices[2] - BlackHole.y) < 25 then
         table.remove(listObjects, i)
@@ -162,16 +184,23 @@ function Game.Update(dt)
     
       if math.abs(o.x - BlackHole.x) < 25 and math.abs(o.y - BlackHole.y) < 25 then
         table.remove(listObjects, i)
+        if o.nature == Game.phase then -- if it is the good phase/dimension
+          createObject(#listObjects + 1, windowWidth, windowHeight, Game.phase + 1) -- ok
+        else
+          createObject(#listObjects + 1, windowWidth, windowHeight, 0) -- else create points
+        end
       end
       
       if o.x > windowWidth + o.boundary or o.x < 0 - o.boundary or
          o.y < 0 - o.boundary or o.y > windowHeight + o.boundary then
           table.remove(listObjects, i)
+          createObject(#listObjects + 1, windowWidth, windowHeight, o.nature) -- ok
       end
       
     end
     
   end
+  Game.timeElapsed = Game.timeElapsed + dt
 end
 
 function Game.Draw()
@@ -184,11 +213,11 @@ function Game.Draw()
   for i = #listObjects, 1, -1 do
     local o = listObjects[i]
     
-    if Game.phase == "0D" then
+    if o.nature == 0 then
       love.graphics.circle("fill", o.x, o.y, o.circSize)
-    elseif Game.phase == "1D" then
+    elseif o.nature == 1 then
       love.graphics.line(o.x, o.y, o.x2, o.y2)
-    elseif Game.phase == "2D" then
+    elseif o.nature == 2 then
       love.graphics.polygon("fill", o.vertices)
       love.graphics.print("x : "..o.vertices[1]..", y : "..o.vertices[2], o.vertices[1], o.vertices[2])
     end
@@ -196,7 +225,8 @@ function Game.Draw()
   end
   
   --love.graphics.printf("angle : "..BlackHole.rotation, 50, 50, 200, "left")
-  love.graphics.printf("nbObjects : "..#listObjects, 50, 50, 200, "left")
+  
+  love.graphics.printf("PHASE "..Game.phase, 10, 10, windowWidth, "left")
   
   love.graphics.setColor(0, 0, 0) -- black
   
